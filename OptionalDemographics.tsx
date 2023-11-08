24247775eb565d5129beb9da40d4184c08c989b6
smahendra14 <115672349+smahendra14@users.noreply.github.com>
2023-11-07 22:16:19 -0600

    Add files via upload

---
+++ OptionalDemographics.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, TextInput, SafeAreaView, TouchableOpacity} from 'react-native'
+
+const OptionalDemographics = (props) => {
+    return (
+        <Text> SAMPLE </Text>
+    )
+};
+
+export default OptionalDemographics;
